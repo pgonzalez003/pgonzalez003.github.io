2bbb9df2dc59aae482fead1cf3d0b7e636fa4ef6
Pat Gonzalez <pgonzalez003@gmail.com>
2020-08-04 15:23:52 -0700

    Second Update

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -31,49 +31,49 @@ $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
 
-$(document).ready(function () {
-  // alert("Yes!");
+// $(document).ready(function () {
+//   // alert("Yes!");
 
-  APIkey = "M3OKahukEnUxjI9OpTx8jCP1zHWQmm1N";
-  // var subject = ["you+the+real+mvp", "cats", "ricky+bobby"];
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=you+the+real+mvp&api_key=" + APIkey +"&limit=1";
+//   APIkey = "M3OKahukEnUxjI9OpTx8jCP1zHWQmm1N";
+//   // var subject = ["you+the+real+mvp", "cats", "ricky+bobby"];
+//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=you+the+real+mvp&api_key=" + APIkey +"&limit=1";
 
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-  }).then(function (response) {
-      console.log(response);
+//   $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//   }).then(function (response) {
+//       console.log(response);
 
-      var results = response.data;
-      for (var i = 0; i < results.length; i++) {
-          console.log(results[i]);
-          var gifImg = $("<img>");
-          var gifDiv = $("<div>")
-          var animated = results[i].images.fixed_height_small.url;
-          var still = results[i].images.fixed_height_small_still.url;
+//       var results = response.data;
+//       for (var i = 0; i < results.length; i++) {
+//           console.log(results[i]);
+//           var gifImg = $("<img>");
+//           var gifDiv = $("<div>")
+//           var animated = results[i].images.fixed_height_small.url;
+//           var still = results[i].images.fixed_height_small_still.url;
 
-          gifImg.attr("src", animated);
-          gifImg.attr("data-still", still);
-          gifImg.attr("data-animate", animated);
-          gifImg.attr("data-state", "animated");
-          gifImg.addClass("gif-image");
+//           gifImg.attr("src", animated);
+//           gifImg.attr("data-still", still);
+//           gifImg.attr("data-animate", animated);
+//           gifImg.attr("data-state", "animated");
+//           gifImg.addClass("gif-image");
 
-          gifDiv.append(gifImg);
+//           gifDiv.append(gifImg);
 
-          $(".gifLanding").append(gifDiv);
-      }
-  });
-})
-$(document).on("click", ".gif-image", function () {
+//           $(".gifLanding").append(gifDiv);
+//       }
+//   });
+// })
+// $(document).on("click", ".gif-image", function () {
 
-  var state = $(this).attr("data-state");
+//   var state = $(this).attr("data-state");
 
-  if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-  } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-  }
-});
+//   if (state === "still") {
+//       $(this).attr("src", $(this).attr("data-animate"));
+//       $(this).attr("data-state", "animate");
+//   } else {
+//       $(this).attr("src", $(this).attr("data-still"));
+//       $(this).attr("data-state", "still");
+//   }
+// });
 
